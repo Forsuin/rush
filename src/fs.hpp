7e0fc6ee446b4fed23ee2993bffd47db795fe4fc
Ian Laidlaw <ian@kaboodles.com>
2024-02-22 14:41:53 -0500

    writing superblock added

--- src/fs.hpp
+++ src/fs.hpp
@@ -1,5 +1,6 @@
 #include <cstdint>
 #include <string>
+#include <fstream>
 
 #include "expected.hpp"
 #include "optional.hpp"
@@ -30,8 +31,16 @@ const int NUM_BLOCK_PTR = 15;
 */
 enum class State : uint16_t
 {
-    OK,
-    ERR,
+    OK = 0,
+    ERR = 1,
+};
+
+#define BYTE_INFO(x) (char *)&x, sizeof(x)
+#define WRITE(ofile, x) ofile.write(BYTE_INFO(x))
+
+struct IFSWritable
+{
+    virtual void write(std::string fs_name, uint32_t block_addr) const = 0;
 };
 
 /*
@@ -40,7 +49,7 @@ enum class State : uint16_t
     and the MBR, but since I don't have to worry about that this will
     be the first block of the FS
 */
-struct Superblock
+struct Superblock : public IFSWritable
 {
     uint32_t num_inodes;
     uint32_t num_blocks;
@@ -52,13 +61,29 @@ struct Superblock
     uint32_t blocks_per_group;
     uint32_t inodes_per_group;
     State state;
+
+    void write(std::string fs_name, uint32_t block_addr) const
+    {
+        std::ofstream ofile(fs_name, std::ios::binary);
+        ofile.seekp((block_addr * (1024 << log_block_size)));
+        WRITE(ofile, num_inodes);
+        WRITE(ofile, num_blocks);
+        WRITE(ofile, num_free_blocks);
+        WRITE(ofile, num_free_inodes);
+
+        WRITE(ofile, log_block_size);
+        WRITE(ofile, blocks_per_group);
+        WRITE(ofile, inodes_per_group);
+        WRITE(ofile, state);
+        ofile.close();
+    }
 };
 
 /*
     Right after the superblock a table of these will describe every block
     in the filesystem
 */
-struct BlockGroupDescriptor
+struct BlockGroupDescriptor : public IFSWritable
 {
     uint32_t block_bitmap_addr;
     uint32_t inode_bitmap_addr;
@@ -66,6 +91,13 @@ struct BlockGroupDescriptor
     uint16_t free_blocks;
     uint16_t free_inodes;
     uint16_t num_dirs;
+
+    void write(std::string fs_name, uint32_t block_addr) const
+    {
+        std::ofstream ofile(fs_name, std::ios::binary);
+
+        ofile.close();
+    }
 };
 
 /*
@@ -83,7 +115,7 @@ enum class FileType : uint16_t
 /*
     Actually points the block containing the file
 */
-struct Inode
+struct Inode : public IFSWritable
 {
     FileType type;
     uint64_t size;
@@ -95,7 +127,7 @@ struct Inode
 /*
     Describes the layout of a entry into a directory
 */
-struct DirEntry
+struct DirEntry : public IFSWritable
 {
     uint32_t inode;
     uint16_t entry_size;
@@ -116,3 +148,6 @@ struct DirEntry
     inode_ratio defaults to 1024 bytes / inode as most of these files should be failry small
 */
 tl::expected<monostate, std::string> mkfs(int fs_size, int block_size, std::string fs_name, int inode_ratio);
+
+#undef WRITE
+#undef BYTE_INFO
